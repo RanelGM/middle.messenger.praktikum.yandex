d0c071e7ffc44b652360c9fe422278d08a6b97c7
RanelGM <mr.kotenkaA@gmail.com>
2024-07-03 13:02:57 +0300

    config: Добавляет поддержку типизации модульного scss

---
+++ src/shared/types/typed-scss.d.ts
@@ -0,0 +1,11 @@
+/* Декларация импортов scss и модульного scss */
+
+declare module "*.scss" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
