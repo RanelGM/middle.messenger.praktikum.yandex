fd28500be3732837acaaa762325d0fb79d33cccb
RanelGM <mr.kotenkaA@gmail.com>
2024-07-27 16:09:35 +0300

    bugfix: Исправляет путь начальной страницы

--- server.ts
+++ server.ts
@@ -1,13 +1,16 @@
+import path from "node:path";
 import express from "express";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static("./dist"));
 
-app.get("/", (_req, res) => {
+app.get("*", (_req, res) => {
   res.status(200);
-  res.send("Приложение создано");
+  res.sendFile(path.join(dirname, "/dist/index.html"));
 });
 
 app.listen(PORT, () => {
