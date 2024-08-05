72e96b6ded4ca1a845953e56a15731d8f6b508b6
RanelGM <mr.kotenkaA@gmail.com>
2024-08-05 10:37:36 +0300

    config: Обновляет referer для JSDOM

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,6 +1,9 @@
 import { JSDOM } from "jsdom";
 
-const jsdom = new JSDOM("<body></body>", { url: "http://localhost:3000/" });
+const jsdom = new JSDOM("<body></body>", {
+  url: "http://localhost/3000",
+  referrer: "http://localhost/3000",
+});
 
 global.window = jsdom.window;
 global.document = jsdom.window.document;
